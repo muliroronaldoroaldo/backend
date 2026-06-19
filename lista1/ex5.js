f4d42cf176936f0b912db20ddc9e906255f4d7f0
muliroronaldoroaldo <murilo.alves.braga@escola.pr.gov.br>
2026-06-19 17:50:50 +0000

    sim

--- lista1/ex5.js
+++ lista1/ex5.js
@@ -2,16 +2,18 @@ const input = require ("readline-sync")
 for(let i=1; i<=100; i++){
 
 
-  
+    if(i % 3 === 0 && i % 5 === 0){
+        console.log(i, "FizzBuzz, ")
+    }
   
     if(i % 3 === 0){
-        console.log(i, "fizz")
+        console.log(i, "Fizz,  ")
     }
     if(i % 5 !== 0 || i % 3 !== 0){
     console.log(i)
     }
     
     if(i % 5 === 0){
-        console.log(i, "buzz")
+        console.log(i, "Buzz, ")
     }
 }
