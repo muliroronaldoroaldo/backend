77f5d36f28df82cda8a9eb7b7d14ebdcccbae747
muliroronaldoroaldo <murilo.alves.braga@escola.pr.gov.br>
2026-06-23 18:47:21 +0000

    tava errado

---
+++ lista1/ex7.js
@@ -0,0 +1,9 @@
+const input = require("readline-sync")
+let soma
+
+for ( let i = 1; i >= 50; i++){
+    if (num % 2 === 0) {
+        total += num
+    }
+} 
+console.log(`a soma dos números pares é:  ${total}`)
