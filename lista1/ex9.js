b8230a267e003fe27fabe5c61e75134c0411e1a2
muliroronaldoroaldo <murilo.alves.braga@escola.pr.gov.br>
2026-06-24 11:53:49 +0000

    ta

--- lista1/ex9.js
+++ lista1/ex9.js
@@ -1,3 +1,12 @@
 const input = require ("readline-sync")
-let numA = input.question()
-let numB = input.question()
+let numA = Number(input.question("coloca o numero 1: "))
+let numB = Number(input.question("coloca o numero 2: "))
+let menor = 0
+
+function menores(numA, numB){
+    if(numA < numB)
+        menor = numA
+    return menor
+}
+
+console.log(menores(numA, numB))
