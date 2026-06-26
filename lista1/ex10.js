ccddd212a4783693e22dc2152012e240a503f2a0
muliroronaldoroaldo <murilo.alves.braga@escola.pr.gov.br>
2026-06-26 11:31:55 +0000

    cu

--- lista1/ex10.js
+++ lista1/ex10.js
@@ -1,12 +1,15 @@
 const input = require ("readline-sync")
+
 let texto = input.question("fala: ")
 let letra = input.question("fala: ")
 let cont = 0
-function ronaldo (texto, letra){
-for(i = 0; i <= texto.lenght; i++){
-    if(texto[i] === letra ){
-        cont++;
+
+function ronaldo(texto, letra){
+    for(let i = 0; i < texto.length; i++){
+        if(texto[i] === letra ){
+            cont++
     }
 }
-
-console.log(`A letra ${letra} aparece ${cont} vezes`);
+return cont ;
+}
+console.log(ronaldo(texto, letra))
