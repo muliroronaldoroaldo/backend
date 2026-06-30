90a4b8433984763b7611429a377b6ffc606f9a25
muliroronaldoroaldo <murilo.alves.braga@escola.pr.gov.br>
2026-06-30 11:43:48 +0000

    m

--- lista1/ex13.js
+++ lista1/ex13.js
@@ -1,7 +1,22 @@
 const input = require ("readline-sync")
-let inicio
-let fim
+let inicio = Number(input.question("escreva um numero: "))
+let fim = Number(input.question("escreva outro numero: "))
 
 function range(incio, fim){
-    
+        let lista = []
+    for(let i = inicio; i <= fim; i++){
+        lista.push(i)
+    }
+    return lista
 }
+
+function soma(lista){
+    let som = 0
+    for(let i = 0; i < lista.length; i++){
+        som += lista[i];
+    }
+    return som
+}
+
+console.log(`${range(inicio, fim)}`)
+console.log(soma(range(inicio, fim)))
